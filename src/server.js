38c4860c89da82c982ec0b3365136b6d5fa97282
HexSeal <maxfin13@gmail.com>
2021-02-25 21:57:25 -0500

    [add] Voting and cleaned up

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(checkAuth);
+app.use(express.static('public'));
 
 // Engine
 app.engine('handlebars', exphbs({
